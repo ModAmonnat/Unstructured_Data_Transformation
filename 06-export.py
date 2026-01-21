52171aa5955c83307523d7eec2e035490a5b9d77
Mod Wongprasert <mod.amonnat@gmail.com>
2026-01-21 00:58:17 -0800

    Update and rename export_file.py to 06-export.py

---
+++ 06-export.py
@@ -0,0 +1,10 @@
+# Set my destination folder
+
+output_folder = r"D:\BCIT\Winter 2026\BI2\CS1"
+
+# Export to Excel
+excel_file = output_folder + r"\merged_transactions_final4.xlsx"                             
+df_final.to_excel(excel_file, index=False)
+print(f"Saved Excel: {excel_file}")
+
+print(f"\nDone! Exported {len(df_final)} transactions")                                                                                                  
